c13ebb5c5aa738a15fe75c79dfbc8766253aff9f
eden6187 <kyh970107@ajou.ac.kr>
2020-10-22 02:37:07 -0700

    parsing

--- assembler.c
+++ assembler.c
@@ -1,20 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 #define LINE_MAX_LEN 512
+#define LABEL_MAX_LEN 32
+#define OPERAND_MAX_LEN 32
+#define OPERATOR_MAX_LEN 32
 
 char* source_path;
 char* object_path;
 
 char line_to_parse[LINE_MAX_LEN];
 
+char label[LABEL_MAX_LEN];
+char operator[OPERATOR_MAX_LEN];
+char operand[OPERAND_MAX_LEN];
+
+int line_idx = 0;
+
 FILE* input_file;
 FILE* output_file;
 
+void read_token(char* line_to_parse, char* place_to_store){
+	int token_idx = 0;
+	while(!(line_to_parse[line_idx] == ' ' || line_to_parse[line_idx] == '\t' || line_to_parse[line_idx] == '\n')){
+		place_to_store[token_idx] = line_to_parse[line_idx];
+		token_idx++;
+		line_idx++;
+	}
+	place_to_store[token_idx] = '\0';
+	return;
+}
+
+void read_blank(char* line_to_parse){
+	while(line_to_parse[line_idx] == ' ' || line_to_parse[line_idx] == '\t')
+		line_idx++;
+}
+
+void parse_line(char* line_to_parse, char* label, char* operator, char* operand){
+	line_idx = 0;
+	read_token(line_to_parse,label);
+	read_blank(line_to_parse);
+	read_token(line_to_parse,operator);
+	read_blank(line_to_parse);
+	read_token(line_to_parse,operand);
+	return;
+}
+
 void do_path1(){
 	while(fgets(line_to_parse,LINE_MAX_LEN,input_file)){
-		printf("%s\n",line_to_parse);
+		if(line_to_parse[0] == '.')
+			continue;
+
+		parse_line(line_to_parse,label,operator,operand);
+		if(strlen(label) + strlen(operator) + strlen(operand) == 0)
+			continue;
+
+		printf("%s %s %s\n",label, operator, operand);
 	}
 }
 
